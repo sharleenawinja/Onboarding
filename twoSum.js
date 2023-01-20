f0f8bc7d6c9b11d5ab19ec4c5bce49dbbc2d5326
SharleenAwinja <awinjasharleen@gmail.com>
2023-01-20 08:34:26 +0300

    sumoftwo

--- twoSum.js
+++ twoSum.js
@@ -5,3 +5,37 @@
 //Given nums = [2, 7, 11, 15], target = 9,
 //Because nums[0] + nums[1] = 2 + 7 = 9,
 //return [0, 1].
+
+function twoSum(nums, target) {
+    let length = nums.length;
+    for(i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+
+        let diffIndex = nums.indexOf(diff);
+        if(diffIndex !== -1 && diffIndex !== i) {
+            return [i, diffIndex];
+        }
+    }
+};
+
+console.log(twoSum([2, 7, 11, 5], 9)); //expected [0, 1]
+console.log(twoSum([3, 2, 4], 6));  //expected [1, 2]
+console.log(twoSum([15, 7, 11, 2], 9)); //expected [1, 3]
+
+//Alternative approach
+const sumOfTwo = (numbers, result) => {
+    let length = numbers.length;
+    for (let i = 0; i < length; i++) {
+        for(let j = i + 1; j < length; j++) {
+            let total = numbers[i] + numbers[j];
+            if(total === result) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+console.log(sumOfTwo([2, 7, 11, 5], 9)); //expected [0, 1]
+console.log(sumOfTwo([3, 2, 4], 6));  //expected [1, 2]
+console.log(sumOfTwo([15, 7, 11, 2], 9)); //expected [1, 3]
+
