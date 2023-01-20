d7901a30c6aa82d41073875809d95cca4dcfc883
SharleenAwinja <awinjasharleen@gmail.com>
2023-01-20 08:30:53 +0300

    sumoftwo

---
+++ twoSum.js
@@ -0,0 +1,7 @@
+//PROBLEM
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may not use the same element twice.
+
+//Given nums = [2, 7, 11, 15], target = 9,
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
